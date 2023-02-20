7664420ac2be7a0be57daf599d3dd1da40fc8560
BryceKnight16 <6373@holbertonstudents.com>
2023-02-19 19:41:25 -0800

    fixing betty coding style

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -17,15 +17,15 @@ int main(void)
 
 	if (n > 0)
 	{
-		printf("%i is  positive\n", n);
+		printf("%i is positive\n", n);
 	}
 	else if (n == 0)
 	{
 		printf("%i is zero\n", n);
 	}
 	else if (n < 0)
-	    {
-		    printf("%i is negative\n", n);
-	    }
+	{
+		printf("%i is negative\n", n);
+	}
 	return (0);
 }

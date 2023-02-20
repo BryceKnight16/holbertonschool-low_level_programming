da1e35d1307ebe19f8bb548f57dd8665833f63f2
BryceKnight16 <6373@holbertonstudents.com>
2023-02-20 15:57:12 -0800

    oops

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char num;
+
+	for (num ='0'; num <= '9'; num++)
+		putchar(num);
+	putchar('\n');
+	return (0);
+}

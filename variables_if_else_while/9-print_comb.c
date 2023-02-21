dd982d799670782fa132b49bb7f855c5a13a2cf1
BryceKnight16 <6373@holbertonstudents.com>
2023-02-20 19:00:29 -0800

    displaying 0, 1 with ascii numbers for spec chara as well as numbers

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * using ascii to print a comma and a space after a number
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char num;
+
+	num = 48; /*demical for 0 */
+	while (num <= 57)/* demical for 9 */
+	{
+		putchar(num);
+		if (num != 57)
+		{
+			putchar (44); /*deicmal for the comma*/
+			putchar (32);/* decimal for space */
+
+		}
+		num = num + 1;
+	}
+	putchar(10);
+	return (0);
+}

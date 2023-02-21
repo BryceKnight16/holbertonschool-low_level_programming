a9d8fe2d1c3234d3cd69801dbdc21a157a00fa27
BryceKnight16 <6373@holbertonstudents.com>
2023-02-20 17:33:45 -0800

    using ascii to display 0-9

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num = 48;
+	while (num <= 57)
+	{
+		putchar(num);
+		num = num + 1;
+	}
+	putchar(10);
+
+	return (0);
+}

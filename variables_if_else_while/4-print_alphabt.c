6b021afc9615c58f4d50505f235596f1782e4409
BryceKnight16 <6373@holbertonstudents.com>
2023-02-20 17:01:24 -0800

    print the alphabet with while and if statements

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * displaying the alphabet without q and e
+ * the while command
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char bet = 'a'
+
+		while (bet < = 'z')
+		{
+			if (bet == 'q' || bet == 'e')
+			{
+				bet = bet + 1
+			}
+			else
+			{
+				putchar(bet);
+			bet = bet + 1;
+		}
+		}
+	putchar('\n');
+
+	return (0);
+		}

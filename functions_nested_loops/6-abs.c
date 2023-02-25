459440486fc70c81ca50cf64b10563f19ffa0720
BryceKnight16 <6373@holbertonstudents.com>
2023-02-24 20:49:10 -0800

    tedsting corrector

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs returns absolute value of a number
+ * @a: An integre input
+ *  Return: Absolut value
+ */
+int _abs(int a)
+{
+	if (a >= 0)
+		return (a);
+	else
+		return (a * -1);
+}

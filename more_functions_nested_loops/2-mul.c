63325a79dd5289c2b0031c6c4dc12157e1579996
BryceKnight16 <6373@holbertonstudents.com>
2023-02-26 04:27:45 -0800

    checking code / betty

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - multiplies two ints
+ * @a: first int to be multied
+ * @b: second into to be multied
+ *
+ *Return: The total of the multiplication
+ */
+
+int mul(int a, int b);
+{
+	return (a * b);
+}

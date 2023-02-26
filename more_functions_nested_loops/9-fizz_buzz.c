0f4e4ff073d624d7e4e67336abdd710f041ca1f0
BryceKnight16 <6373@holbertonstudents.com>
2023-02-26 06:11:21 -0800

    tested locally and worked completed code

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry piont
+ * Return: Always 0
+ */
+
+
+int main(void)
+{
+	int i = 1;
+
+
+	while (i < 100)
+	{
+
+		if (i % 3 == 0 && i % 5 == 0)
+
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+		i = i + 1;
+	}
+	printf("Buzz\n");
+	return (0);
+}

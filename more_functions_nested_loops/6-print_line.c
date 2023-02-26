d50bd10d61d6f40ae196ce1606848f1aa1495388
BryceKnight16 <6373@holbertonstudents.com>
2023-02-26 05:21:31 -0800

    checking betty and code

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - print the character "_" n times to draw a line
+ * @n: number of characters to draw
+ */
+
+void print_line(int n)
+void print_line(int n)
+{
+	int times = n;
+
+	while (times > 0)
+	{
+	_putchar('_');
+	times = times - 1;
+	}
+	_putchar('\n');
+}

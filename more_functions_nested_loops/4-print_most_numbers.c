a089d11b6b33e0f568c21e60ffaf45f64faaed71
BryceKnight16 <6373@holbertonstudents.com>
2023-02-26 06:30:14 -0800

    printing all numbers but 2 and 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print 0 to 9, skip 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	char c;
+
+	c = 0;
+
+	while (c <= 9)
+	{
+		if (c != '2' && i != '4')
+			_putchar(c + '0');
+		c = c + 1;
+	}
+	_putchar('\n');
+}

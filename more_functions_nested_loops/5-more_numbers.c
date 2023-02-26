2f2785aa18606ed776ac42aafa584c3bf2d542fc
BryceKnight16 <6373@holbertonstudents.com>
2023-02-26 05:04:14 -0800

    checking code and betty

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -6,18 +6,21 @@
 
 void more_numbers(void)
 {
-    int i = 0, j = 0;
+	int i = 0, j = 0;
 
-    while (j < 10) {
-        i = 0;
-        while (i <= 14) {
-            if (i > 9) {
-                _putchar(i / 10 + '0');
-            }
-            _putchar(i % 10 + '0');
-            i = i + 1;
-        }
-        _putchar('\n');
-        j = j + 1;
-    }
+	while (j < 10)
+	{
+		i = 0;
+		while (i <= 14)
+		{
+			if (i > 9)
+			{
+				_putchar(i / 10 + '0');
+			}
+			_putchar(i % 10 + '0');
+			i = i + 1;
+		}
+		_putchar('\n');
+		j = j + 1;
+	}
 }

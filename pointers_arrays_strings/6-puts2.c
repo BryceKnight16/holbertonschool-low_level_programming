b0e5b022e1eaafecd475d59a15d5d139241c1273
BryceKnight16 <6373@holbertonstudents.com>
2023-03-01 03:50:41 -0800

    code locally tested and worked, testing with checker now

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len = len + 1;
+		s += 1;
+	}
+
+	return (len);
+}
+
+/**
+ * puts2 - prints every second character of a string, followed by a new line
+ * @str: string to print
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0' && i < _strlen(str))
+	{
+	    _putchar(str[i]);
+	    i += 2;
+	}
+
+	_putchar('\n');
+}

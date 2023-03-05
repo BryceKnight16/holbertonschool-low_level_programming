7f1b56cae12842300905be5be87c414d06a9bd66
BryceKnight16 <6373@holbertonstudents.com>
2023-03-05 03:58:42 -0800

    tested locally tested and worked adding everyting to github

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * _memset - Fill n bytes of memory area pointed to with constant byte
+ * @s: pointer
+ * @b: byte to fill
+ * @n: Amount of byte to add
+ *
+ * Return: Pointer to memory area
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n)
+	{
+		s[i] = b;
+		i= i + 1;
+	}
+	return (s);
+}

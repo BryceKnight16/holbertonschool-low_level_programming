9f4744c102bc4814bcc4cf38d7237c7c4d2c6e80
BryceKnight16 <6373@holbertonstudents.com>
2023-03-05 03:59:25 -0800

    tested locally tested and worked adding everyting to github

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strchr - Find given char in a string
+ * @s: string to search
+ * @c: char to search for
+ *
+ * Return: Pointer to first c
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		else
+			s= s + 1;
+	}
+	if (*s == c)
+		return (s);
+	else
+		return ('\0');
+}

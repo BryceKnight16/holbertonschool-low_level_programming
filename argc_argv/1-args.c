3e465b6c5879a7b27a6e8d709911a6e831612a88
BryceKnight16 <6373@holbertonstudents.com>
2023-03-09 19:16:35 -0800

    counting the number the arguments on command line

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - counts number of arguments
+* @argc: argument counter
+* @argv: pointer to array of arguments
+* Return: the total number of argc
+*/
+int main(int argc, char **argv)
+{
+
+	argv = argv;
+
+	printf("%d\n", argc - 1); 
+        return (0);
+}

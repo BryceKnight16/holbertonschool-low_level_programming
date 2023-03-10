8119ddfea6f0f906d79bbd87371ea24348ef5972
BryceKnight16 <6373@holbertonstudents.com>
2023-03-09 21:07:55 -0800

    adding prototypes for att unused for both argc and argv, fixing argc

--- argc_argv/main.h
+++ argc_argv/main.h
@@ -1,4 +1,4 @@
 int _putchar(char c);
 int main(int argc, char **argv);
-int main(int argc, __attribute__((unused)) char **argv);
+int main(int argc __attribute__((unused)), char **argv);
 int main(int argc, char **argv __attribute__((unused)));

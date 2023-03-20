51a896f41dcf7bc660009e070733514fb50ee5c4
BryceKnight16 <6373@holbertonstudents.com>
2023-03-20 16:47:45 -0700

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - attributes of a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: details of the dog
+ */
+
+struct dog
+{
+	char *owner;
+	char *name;
+	float age;
+};
+#endif

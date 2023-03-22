a292ded2aff9163ec6f2aadbf779dd2508e1a22c
BryceKnight16 <6373@holbertonstudents.com>
2023-03-21 22:04:46 -0700

    hello Abija-Joyce, making a calculator for just you :)

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -9,14 +9,14 @@ int main (int argc, char **argv)
 	int num2;
 	int result;
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
 	result = get_op_func(argv[2])(num1, num2);
 	printf("%d\n", result);

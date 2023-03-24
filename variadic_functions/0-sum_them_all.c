bfade0b9270ed1400eebad26de6ee1b57c8d98dc
BryceKnight16 <6373@holbertonstudents.com>
2023-03-23 20:46:40 -0700

    a function adds all of the parameters

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+int sum_them_all (const unsigned int n,...)
+{
+	va_list ap;
+	unsigned int i;
+	int sum;
+
+	va_start (ap, n);
+
+	if (n == 0)
+	{
+		return(0);
+	}
+
+	i = 0;
+	sum = 0;
+
+	while (i < n)
+	{
+		sum = sum + va_arg (ap, int);
+		i = i + 1;
+	}
+
+	va_end (ap);
+	return (sum);
+}

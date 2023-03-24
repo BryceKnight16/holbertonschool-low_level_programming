87ecbcb9c35ce28d4387160ee9d9d39be0554e58
BryceKnight16 <6373@holbertonstudents.com>
2023-03-23 23:16:28 -0700

    updating the header file with the prototype, and a function that prints numbers

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,38 @@
+#include <stddef.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - prints numbers
+ * @separator: string to be printed between numbers
+ * @n: number of ints passed to the function
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+
+	va_list ap;
+	unsigned int i;
+
+	if (separator == NULL)
+	{
+		return;
+	}
+
+	va_start(ap,n);
+	i = 0;
+
+	while (i < n)
+	{
+		if(i != 0)
+		{
+			printf("%s", separator);
+		}
+		printf("%d", va_arg(ap, int));
+		i = i + 1;
+	}
+	printf("\n");
+	va_end(ap);
+}

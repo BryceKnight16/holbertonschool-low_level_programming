b4a4f480cbbc3956ba022b3a2a0d34836217000d
BryceKnight16 <6373@holbertonstudents.com>
2023-03-23 21:33:07 -0700

    a function that searches for an integer in an array

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	i = 0;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+
+	while (i < size)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return(i);
+		}
+		i = i + 1;
+	}
+	return (-1);
+}

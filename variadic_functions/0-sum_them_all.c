b1a6684fd1d3dba700d919022ad876f77f795f66
BryceKnight16 <6373@holbertonstudents.com>
2023-03-26 03:48:10 -0700

    fixing betty

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 
-int sum_them_all (const unsigned int n,...)
+int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i;
 	int sum;
 
-	va_start (ap, n);
+	va_start(ap, n);
 
 	if (n == 0)
 	{
-		return(0);
+		return (0);
 	}
 
 	i = 0;
@@ -20,10 +20,10 @@ int sum_them_all (const unsigned int n,...)
 
 	while (i < n)
 	{
-		sum = sum + va_arg (ap, int);
+		sum = sum + va_arg(ap, int);
 		i = i + 1;
 	}
 
-	va_end (ap);
+	va_end(ap);
 	return (sum);
 }

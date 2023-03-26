44272dd01aa15423a76c209d8ce09217228bc8a8
BryceKnight16 <6373@holbertonstudents.com>
2023-03-26 05:37:56 -0700

    writing a function that checks the type ofthe contains of a string and then prints all of it

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,97 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+
+/**
+ *print_char - prints a char
+ *@ap: va_list type
+ *Return: void
+ */
+
+void print_char(va_list ap)
+{
+	printf("%c", va_arg(ap, int));
+}
+
+/**
+ *print_int - prints an int
+ *@args: va_list type to iterate through variable arguments
+ *Return: void
+ */
+
+void print_int(va_list ap)
+{
+	printf("%d", va_arg(ap, int));
+}
+
+/**
+ *print_float - prints a float
+ *@args: va_list type to iterate through variable arguments
+ *Return: void
+ */
+
+void print_float(va_list ap)
+{
+	printf("%f", va_arg(ap, double));
+}
+
+/**
+ *print_str - prints a string
+ *@args: va_list type to iterate through variable arguments
+ *Return: void
+ */
+
+void print_str(va_list ap)
+{
+	char *str = va_arg(ap, char *);
+
+	str == NULL ? printf("(nil)") : printf("%s", str);
+}
+
+/**
+ * print_all - prints anything
+ * @format: is a list of types of arguments passed to the function
+ * Return: void
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list ap;
+	int i;
+	int a;
+
+	print_all_t check[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_str},
+		{NULL, NULL}
+	};
+
+	va_start(ap, format);
+	i = 0;
+
+	while (format != NULL && format[i] != '\0')
+	{
+		a = 0;
+
+		while (check[a].op != NULL)
+		{
+			if (*check[a].op == format[i])
+			{
+				(check[a].f)(ap);
+
+				if (format[i + 1] != '\0')
+				{
+					printf(", ");
+				}
+			}
+			a = a + 1;
+		}
+		i = i + 1;
+	}
+	va_end(ap);
+	printf("\n");
+}

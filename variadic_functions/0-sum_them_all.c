9d4407d432846f98d9ba6afef6baeb5d4bd0306d
BryceKnight16 <6373@holbertonstudents.com>
2023-03-26 03:51:51 -0700

    fixing betty

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -2,6 +2,13 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 
+/**
+ * sum_them_all - returns the sum of a variable number of integers
+ * @n: the number of integers to sum
+ *
+ * Return: answer or 0 if n is 0
+ */
+
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;

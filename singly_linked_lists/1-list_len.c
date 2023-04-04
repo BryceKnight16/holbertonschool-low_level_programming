c052822d2e4223d640c283f64cf2ba919af3a7a7
BryceKnight16 <6373@holbertonstudents.com>
2023-04-03 17:36:07 -0700

    a function that lists all elements of struct

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ *list_len - counts the elements in list_t
+ *@h: pointer to list_t data type
+ *
+ *Return: number of elements in list_t
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t size = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		size = size + 1;
+	}
+	return (size);
+}

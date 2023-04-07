40885d97ca9082adb8060a7f47a72d771ab24aea
BryceKnight16 <6373@holbertonstudents.com>
2023-04-07 01:01:52 -0700

    converting only 1s and  0s into binary

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * is_binary - checks if char is 0 or 1
+ * @c: the character
+ * Return: 1 for  binary char, 0 for other.
+ */
+
+int is_binary(char ch)
+{
+	if (ch == '1' || ch == '0')
+	{
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * binary_to_uint - changes binary chars to a number
+ *
+ * @b: string containing chars of a binary number
+ * Return: the number as an unsigned int
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result;
+	int index;
+
+	index = 0;
+	result = 0;
+
+	while (b[index] != '\0')
+	{
+		if (is_binary(b[index]) == 0)
+		{
+			return (0);
+		}
+
+		result = result << 1;
+
+		if (b[index] == '1')
+		{
+			result = result | 1;
+		}
+		index = index + 1;
+	}
+	return (result);
+}

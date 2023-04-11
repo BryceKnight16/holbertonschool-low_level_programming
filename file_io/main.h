64288286bc9acaface032a3945ee9608e9560040
BryceKnight16 <6373@holbertonstudents.com>
2023-04-10 22:11:39 -0700

    adding main.h and README.md

---
+++ file_io/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <fcntl.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+
+
+#endif

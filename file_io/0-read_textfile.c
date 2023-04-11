3b060fbdac2d22c0fbd8eca480b42e0291a63d78
BryceKnight16 <6373@holbertonstudents.com>
2023-04-10 22:18:37 -0700

    a function that reads a text file that then rpints the POSIX to the stdoutput

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *storedlets;
+	int fd;
+	ssize_t let_counter;
+	ssize_t printed_let;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	fd = open(filename, O_RDWR);
+
+	if (fd == -1)
+	{
+		return (0);
+	}
+
+	storedlets = malloc(sizeof(*storedlets) *letters);
+
+	if(storedlets == NULL)
+	{
+		return (0);
+	}
+
+	let_counter = read(fd, storedlets, letters);
+
+	if (let_counter == -1)
+		{
+			return (0);
+		}
+
+	printed_let = write(STDOUT_FILENO, storedlets, let_counter);
+
+	if (printed_let == -1 || printed_let != let_counter)
+	{
+		return (0);
+	}
+	free(storedlets);
+	close(fd);
+	return (printed_let);
+}

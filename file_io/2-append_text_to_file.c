c9ca04414e6052b2e921eaf78e9135e2ad49028f
BryceKnight16 <6373@holbertonstudents.com>
2023-04-11 20:36:03 -0700

    updating the task 1 and the main.h and writing a function that can append a file

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * create_file - Creates a file with the filename and text content
+ * append_text_to_file - Creates a file with the filename and text content
  * @filename: Name of the file to create
  * @text_content: string to write to the file
  *
  * Return: 1 on success, -1 on failure
  */
-int create_file(const char *filename, char *text_content)
+
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
 	int chars_written;
@@ -16,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (fd == -1)
 	{
@@ -28,9 +29,12 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		while (text_content[counter] != '\0')
+		{
 			counter = counter + 1;
+		}
 
 		chars_written = write(fd, text_content, counter);
+
 		if (chars_written == -1)
 		{
 			close(fd);

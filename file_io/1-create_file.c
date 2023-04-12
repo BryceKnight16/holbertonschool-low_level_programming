ce59e245983c48e7046dcab9aa94ef354d354ccb
BryceKnight16 <6373@holbertonstudents.com>
2023-04-11 20:15:48 -0700

    updating main.h with a new the prototype and writing a function that creates a file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file with the filename and text content
+ * @filename: Name of the file to create
+ * @text_content: string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int bytes_written;
+	int counter;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	counter = 0;
+
+	if (text_content != NULL)
+	{
+		while (text_content[counter] != '\0')
+			counter = counter + 1;
+
+		bytes_written = write(fd, text_content, counter);
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
--- file_io/main.h
+++ file_io/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <fcntl.h>
 #include <stddef.h>
 #include <stdlib.h>
@@ -9,7 +10,7 @@
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
-
+int create_file(const char *filename, char *text_content);
 
 
 

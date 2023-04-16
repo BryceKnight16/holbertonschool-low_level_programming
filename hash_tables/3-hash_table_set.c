dd41cdfffc72e26ccd360ca47a02fb4474313768
BryceKnight16 <6373@holbertonstudents.com>
2023-04-16 04:50:01 -0700

    fixing memory leak i i think

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -61,6 +61,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(key, ht->array[index]->key) == 0)
 		{
+			free(ht->array[index]->value);
 			ht->array[index]->value = (char *)value;
 			return (1);
 		}
